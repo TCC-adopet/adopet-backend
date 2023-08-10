edab1a6eb96b9b07115307cebfe53c78f8d0c700
XD4ngerX <gustavo08dias@gmail.com>
2023-08-10 17:41:04 -0300

    Create adocao.cs

---
+++ Models/adocao.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace AdopetMeApi.Models
+{
+    public class adocaoApi{
+    [Key]public string? idAdocao { get; set; }
+    public string? idPedido { get; set; }
+    public string? petAdotado { get; set; }
+    public DateTime? dataAdocao { get; set; }
+    public DateTime? dataRecusa { get; set; }
+    public string? idAnimal { get; set; }
+    }
+    public class adocaoUploadApi{
+    public string? idPedido { get; set; }
+    public string? petAdotado { get; set; }
+    public DateTime? dataAdocao { get; set; }
+    public DateTime? dataRecusa { get; set; }
+    public string? idAnimal { get; set; }
+    }
+    public class adocaoUpdateApi{
+    public string? petAdotado { get; set; }
+    public DateTime? dataAdocao { get; set; }
+    public DateTime? dataRecusa { get; set; }
+    }
+}
