ed5517cfddbf7a2ee5ec78716075782292739298
XD4ngerX <gustavo08dias@gmail.com>
2023-09-07 15:10:04 -0300

    .

--- models/model.js
+++ models/model.js
@@ -1,8 +1,20 @@
 var mongoose = require("mongoose");
 
 var UserSchema = new mongoose.Schema({
-  name: {required: true, type: String},
+  nome: {required: true, type: String},
+  sobreNome: {required: true, type: String},
   age: {required: true, type: Number},
+  codCpf: {required: true, type: Number},
+  idCidade: {required: true, type: Number},
+  bairro: {required: true, type: String},
+  rua: {required: true, type: String},
+  numeroCasa: {required: true, type: Number},
+  codCEP: {required: true, type: Number},
+  sexo: {required: true, type: String},
+  apelido: {required: true, type: String},
+  numeroTelefone: {required: true, type: Number},
+  senha: {required: true, type: String},
+  dataNascimento: {required: true, type: Date}
 });
 
 var Users = mongoose.model("Users", UserSchema);
