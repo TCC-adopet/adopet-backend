056f8120905ccf06fa45a4be882b016c78f0ece3
XD4ngerX <gustavo08dias@gmail.com>
2023-11-14 15:27:24 -0300

    .

--- middleware/fotos.js
+++ middleware/fotos.js
@@ -5,7 +5,7 @@ var uploadFoto = () => {
     return multer({
         storage: multer.diskStorage({
             destination: function (req, file, cb) {
-                cb(null, 'uploads/')
+                cb(null, 'uploads/fotos')
             },
             filename: function (req, file, cb) {
                 var clientId = req.params.id; // Acessando o ID do cliente a partir dos parâmetros da URL
@@ -29,4 +29,31 @@ var uploadFoto = () => {
     });
 };
 
-module.exports = uploadFoto;
+function getUploadedFotos(req, res) {
+    const uploadsDirectory = 'uploads/fotos';
+    var uploadedFiles = [];
+    var splited = [];
+    var id = req.params.id;
+
+    // Verificando se a pasta 'uploads/perfil' existe
+    if (!fs.existsSync(uploadsDirectory)) {
+        fs.mkdirSync(uploadsDirectory);
+    }
+
+    // Lendo os arquivos da pasta 'uploads/'
+    fs.readdirSync(uploadsDirectory).forEach(file => {
+        
+        splited = file.split('.');
+        splited = splited[0].split('-');
+        if(splited[0] == id)
+        // Adicionando os nomes dos arquivos na lista
+        uploadedFiles.push(file);
+    });
+
+    return uploadedFiles;
+}
+
+module.exports = {
+    uploadFoto,
+    getUploadedFotos
+};
--- middleware/perfil.js
+++ middleware/perfil.js
@@ -1,10 +1,12 @@
 var multer = require('multer');
+var fs = require('fs');
+var path = require('path');
 
 var uploadFotoPerfil = () => {
     return multer({
         storage: multer.diskStorage({
             destination: function (req, file, cb) {
-                cb(null, 'uploads/')
+                cb(null, 'uploads/perfil')
             },
             filename: function (req, file, cb) {
                 var clientId = req.params.id; // Acessando o ID do cliente a partir dos parâmetros da URL
@@ -21,4 +23,30 @@ var uploadFotoPerfil = () => {
     });
 };
 
-module.exports = uploadFotoPerfil;
+function getUploadedFotosPerfil(req, res) {
+    const uploadsDirectory = 'uploads/perfil';
+    var uploadedFiles = [];
+    var splited = [];
+    var id = req.params.id;
+
+    // Verificando se a pasta 'uploads/perfil' existe
+    if (!fs.existsSync(uploadsDirectory)) {
+        fs.mkdirSync(uploadsDirectory);
+    }
+
+    // Lendo os arquivos da pasta 'uploads/'
+    fs.readdirSync(uploadsDirectory).forEach(file => {
+        
+        splited = file.split('.');
+        if(splited[0] == `${id}-Perfil`)
+        // Adicionando os nomes dos arquivos na lista
+        uploadedFiles.push(file);
+    });
+
+    return uploadedFiles;
+}
+
+module.exports ={
+    getUploadedFotosPerfil,
+    uploadFotoPerfil
+};
